4fd1601aa55791b96df3417055a3032139d65e79
MIC-RADIOLOGY <travorjamela00@gmail.com>
2026-02-13 07:22:14 +0200

    Add lessons data for language learning

---
+++ data.py
@@ -0,0 +1,30 @@
+lessons = [
+    {
+        "title": "Greetings",
+        "content": [
+            ("Bonjour", "Hello"),
+            ("Bonsoir", "Good evening"),
+            ("Salut", "Hi"),
+            ("Au revoir", "Goodbye")
+        ]
+    },
+    {
+        "title": "Numbers",
+        "content": [
+            ("Un", "One"),
+            ("Deux", "Two"),
+            ("Trois", "Three"),
+            ("Quatre", "Four"),
+            ("Cinq", "Five")
+        ]
+    },
+    {
+        "title": "Common Phrases",
+        "content": [
+            ("Merci", "Thank you"),
+            ("S'il vous plaît", "Please"),
+            ("Excusez-moi", "Excuse me"),
+            ("Je ne comprends pas", "I don’t understand")
+        ]
+    }
+]
